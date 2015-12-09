d29afa55588b5ba66845e6222c23e60c3a3079fb
Logan <lwhit@vt.edu>
2015-12-09 18:48:29 -0500

    Basic structure in place

--- NewsGetter/ViewController.swift
+++ NewsGetter/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url1 = NSURL(string: "http://www.stackoverflow.com")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url1) { (data, response, error) -> Void in
+            //executes when task completes
+            
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                print(webContent)
+            
+            } else {
+                //show error message
+            }
+         }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
