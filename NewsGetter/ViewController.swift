2c3a1e98dfe61ffc1492698602cbab9d667f7fa9
Logan <lwhit@vt.edu>
2015-12-09 21:53:48 -0500

    All but showing web page

--- NewsGetter/ViewController.swift
+++ NewsGetter/ViewController.swift
@@ -8,23 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var userSearch: UITextField!
+    @IBOutlet var webView: UIWebView!
+    var searchableContent: NSString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let url = NSURL(string: "https://news.google.com/")!
+        let urlStandard = NSURL(string: "https://blank.org/")!
         
-        let url1 = NSURL(string: "http://www.stackoverflow.com")!
+        self.userSearch.delegate = self
         
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url1) { (data, response, error) -> Void in
+        //blank view when we first load app
+        webView.loadRequest(NSURLRequest(URL: urlStandard))
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
             //executes when task completes
             
             if let urlContent = data {
                 let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
-                print(webContent)
-            
+                //print(webContent)
+                self.searchableContent = webContent!
             } else {
                 //show error message
+                NSLog("Could not find web content")
+                
             }
          }
         
@@ -34,6 +45,41 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        //if we click outside of the keyboard, the keyboard closes
+        
+        NSLog("hello")
+        self.view.endEditing(true) //true... force?
+        
+    }
+    
+    func textFieldShouldReturn( textField: UITextField ) -> Bool {
+        //if we push return, we want the keyboard to close
+        userSearch.resignFirstResponder()
+        
+        executeSearch()
+        return true //completed successfully
+    }
+    
+    func executeSearch() -> Bool {
+        print("enter rec.")
+        NSLog("enter recognized")
+        //runs when enter is pressed while editing search field
+        let userInput = NSString(string: userSearch.text!)
+        if (searchableContent.containsString(userInput as String) ) {
+            //if what the user is trying to search for is found on
+            //the code we get back from downloading data from
+            //the news website, we try to find the url for it to display
+            print("found what the user is searching for!")
+        } else {
+            print("could not find term")
+        }
+        
+        
+        
+        return true
     }
 
 
