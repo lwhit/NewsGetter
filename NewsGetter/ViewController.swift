9f04edb6fa98b0ddc3c96f9a7a1f9fd5a5b229b0
Logan <lwhit@vt.edu>
2015-12-09 23:29:19 -0500

    Final product

--- NewsGetter/ViewController.swift
+++ NewsGetter/ViewController.swift
@@ -12,18 +12,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var userSearch: UITextField!
     @IBOutlet var webView: UIWebView!
     var searchableContent: NSString = ""
+    var url = NSURL(string: "https://news.google.com/")!
+    var displayedURL = NSURL(string: "https://blank.org/")!
+    let urlStandard = NSURL(string: "https://blank.org/")!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        let url = NSURL(string: "https://news.google.com/")!
-        let urlStandard = NSURL(string: "https://blank.org/")!
+        self.webView.scalesPageToFit = true
+        self.webView.contentMode = UIViewContentMode.ScaleAspectFit
         
         self.userSearch.delegate = self
+        updateDisplay()
+    }
+    
+    func updateDisplay() -> Bool {
+        //self.userSearch.delegate = self
         
-        //blank view when we first load app
-        webView.loadRequest(NSURLRequest(URL: urlStandard))
+        //update webView
+        webView.loadRequest(NSURLRequest(URL: displayedURL))
         
         let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
             //executes when task completes
@@ -37,9 +45,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 NSLog("Could not find web content")
                 
             }
-         }
+        }
         
         task.resume()
+        return true
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,7 +59,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         //if we click outside of the keyboard, the keyboard closes
         
-        NSLog("hello")
+        //NSLog("hello")
         self.view.endEditing(true) //true... force?
         
     }
@@ -64,21 +73,47 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     func executeSearch() -> Bool {
-        print("enter rec.")
-        NSLog("enter recognized")
         //runs when enter is pressed while editing search field
         let userInput = NSString(string: userSearch.text!)
         if (searchableContent.containsString(userInput as String) ) {
             //if what the user is trying to search for is found on
             //the code we get back from downloading data from
             //the news website, we try to find the url for it to display
-            print("found what the user is searching for!")
+            //print("found what the user is searching for!")
+            //now we must find the url to the story that the user searched for so we can display it in the app
+            
+            //let charRange = searchableContent.rangeOfString(userInput as String)
+            
+            //let searchRange = NSMakeRange(charRange.location - 1000, 2000 )
+            
+            //print("\(userInput) found at index \(charRange.location) and is \(charRange.length) chars long")
+            
+            //let searchForURL: NSString = searchableContent.substringWithRange(searchRange)
+            //var newURL: NSURL
+            
+            //if (searchForURL.containsString("url=")) {
+                //aha. Found the URL
+              //  let startofURL = searchableContent
+                
+               // newURL = NSURL(string: searchableContent.substringWithRange(urlRange))!
+                //displayedURL = newURL
+            //} else {
+               // NSLog("could not find url...")
+            //}
+            displayedURL = NSURL(string: "https://memecrunch.com/meme/AFUA3/found-it/image.jpg?w=1024&c=1")!
+            
+            
         } else {
             print("could not find term")
+            displayedURL = urlStandard;
+            
         }
         
         
-        
+        if (updateDisplay() ) {
+            //display updated correctly
+            NSLog("Display Correct")
+        }
         return true
     }
 
---
+++ Readme.txt
@@ -0,0 +1,17 @@
+Project by Logan Whitmire
+Completed on 9 December 2015
+
+Purpose: To search Google News for a key word. If the key word is found on 
+the website, then a picture of a dog informs you of this.
+If we can not find the key word on Google News, then you get a blank page :/
+
+Original intention of the project could not be met on time. Original intention
+was to display the link given for a story that corresponds with the key word.
+
+Unfortunately, could not make this happen. I was still able to provide user
+feedback though by displaying different websites depending on the actions
+of the program. 
+
+Running instructions: Open project in XCode and run with simulator. I ran it
+using an iPhone 6 simulator, the display has constraints though so it should
+display properly in any setting. 
